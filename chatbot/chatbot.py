8cc04a3d8a38bec337ddb5205a16d01b24f35091
Austen Cabret <austencabret@gmail.com>
2023-07-09 21:56:59 -0400

    made folder names snake case

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -1,3 +1,20 @@
+import openai
+
+
+
+
+
+openai
+
+
+
+
+# pip install openai with a virtual environment
+# get the api key from openai's site
+
+
+
+
 # setup openAI's api
 # fine tune it on the building code
 # setup input method
