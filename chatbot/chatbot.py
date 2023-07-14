ddaaa75bfcc8262a42a4b1f4f1954e2028e07303
Austen Cabret <austencabret@gmail.com>
2023-07-14 03:33:37 -0400

    added to the dataset to increase chatbot accuracy

--- chatbot/chatbot.py
+++ chatbot/chatbot.py
@@ -12,7 +12,7 @@ openai
 # openai tools fine_tunes.prepare_data -f <LOCAL_FILE>
 
 # pip install openai with a virtual environment
-key = "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"
+key = "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"
 
 # create a fine tune model
 # openai api fine_tunes.create -t ./datasets/municode_florida_west_palm_beach_chapter94_zoning_and_land_development_regulations.json -m curie
@@ -22,9 +22,9 @@ key = "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"
 # use either python or openai cli to make a completion request
 
 # python
-openai.Completion.create(
-    model="curie:ft-personal-2023-07-12-09-41-49",
-    prompt="What is section 94-76?")
+# openai.Completion.create(
+#     model="curie:ft-personal-2023-07-12-09-41-49",
+#     prompt="What is section 94-76?")
 
 # openai cli
 # openai api completions.create -m <FINE_TUNED_MODEL> -p <YOUR_PROMPT>
@@ -33,7 +33,7 @@ openai.Completion.create(
 # delete a fine-tune model with either python or openai cli
 # python
 # import openai
-# openai.Model.delete(FINE_TUNED_MODEL)
+openai.Model.delete("curie:ft-personal-2023-07-12-09-41-49")
 
 # openai cli
 # openai api models.delete -i <FINE_TUNED_MODEL>
